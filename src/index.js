84e584f4884e1fa1401bc601c90dc8cb21b1ed0d
Naman Arora <46819389+naman3112@users.noreply.github.com>
2023-11-12 18:35:08 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,100 @@
+import "./styles.css";
+
+(async function () {
+  const data = await fetch("./src/data.json");
+  const res = await data.json();
+  let employees = res;
+  let selectedEmployeeId = employees[0].id;
+  let selectedEmployee = employees[0];
+  const employeeList = document.querySelector(".employees__names--list");
+  const employeeInfo = document.querySelector(".employees__single--info");
+  // Add employee loigc
+  const createEmployee = document.querySelector(".createEmployee");
+  const addEmployeeModal = document.querySelector(".addEmployee");
+  const addEmployeeForm = document.querySelector(".addEmployee_create");
+  createEmployee.addEventListener("click", () => {
+    addEmployeeModal.style.display = "flex";
+    console.log("i am clickecd");
+  });
+
+  addEmployeeModal.addEventListener("click", (e) => {
+    if (e.target.className === "addEmployee") {
+      addEmployeeModal.style.display = "none";
+    }
+  });
+
+  const dobInput = document.querySelector(".addEmployee_create--dob");
+  dobInput.max = `${
+    new Date().getFullYear() - 18
+  }-${new Date().toISOString().slice(5, 10)}`;
+
+  addEmployeeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(addEmployeeForm);
+    const values = [...formData.entries()];
+    let empData = {};
+    values.forEach((val) => {
+      empData[val[0]] = val[1];
+    });
+
+    empData.id = employees[employees.length - 1].id + 1;
+    empData.age =
+      new Date().getFullYear() - parseInt(empData.dob.slice(0, 4), 10);
+    empData.imageUrl =
+      empData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png";
+    employees.push(empData);
+    renderEmployees();
+    addEmployeeForm.reset();
+    addEmployeeModal.style.display = "none";
+
+    console.log(values);
+  });
+  // select employee logic
+
+  employeeList.addEventListener("click", (e) => {
+    console.log(
+      "target work is ",
+      e.target.tagName,
+      e.target.id,
+      selectedEmployeeId
+    );
+    if (e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id) {
+      selectedEmployeeId = e.target.id;
+
+      renderEmployees();
+      renderSingleEmployee();
+    }
+  });
+  const renderEmployees = () => {
+    employeeList.innerHTML = "";
+    employees.forEach((emp) => {
+      const employee = document.createElement("span");
+      employee.classList.add("employees__names--item");
+
+      if (parseInt(selectedEmployeeId, 10) === emp.id) {
+        employee.classList.add("Selected");
+        console.log("i am selected here");
+        selectedEmployee = emp;
+      }
+      employee.setAttribute("id", emp.id);
+      employee.innerHTML = `${emp.firstName}  ${emp.lastName} <i class="employeeDelete">&#x1F5D1;</i>`;
+      employeeList.append(employee);
+    });
+  };
+  // redner single emplotyee
+  const renderSingleEmployee = () => {
+    employeeInfo.innerHTML = `<img src="${selectedEmployee.imageUrl}"/>
+    <span class="employees__single--heading"> ${selectedEmployee.firstName} ${selectedEmployee.lastName}  </span>
+    <span>${selectedEmployee.address}</span>
+    <span>${selectedEmployee.email}</span>
+    <span>Mobile - ${selectedEmployee.contactNumber}</span>
+    <span>DOB - ${selectedEmployee.dob}</span>
+
+    `;
+  };
+  if (selectedEmployee) {
+    renderSingleEmployee();
+  }
+  renderEmployees();
+  console.log("data is ", data);
+})();
